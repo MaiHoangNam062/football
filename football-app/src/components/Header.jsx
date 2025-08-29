779310c21e445629e66a92aa715c744bc9d7a0c5
Nam Mai <maihoangnam.study@gmail.com>
2025-08-29 22:18:57 +0200

    Add Header

--- football-app/src/components/Header.jsx
+++ football-app/src/components/Header.jsx
@@ -1,9 +1,20 @@
-function Header() {
-  return (
-    <header className="bg-gray-800 text-white p-4 shadow-md">
-      <h1 className="text-2xl font-bold text-center">My App Header</h1>
-    </header>
-  )
-}
+import React from "react";
+import logo from "../images/logo.png";
 
-export default Header
+const Header = () => {
+  return (
+    <header className="bg-[#454952] text-white p-8 shadow-md flex items-center">
+      <img src={logo} alt="Logo" className="h-10 w-auto" />
+      <h1 className="ml-4 text-xl font-semibold">Football Market</h1>
+
+      <nav className="space-x-10 ml-auto">
+        <a href="" className="hover:text-gray-300">Home</a>
+        <a href="" className="hover:text-gray-300">Log In</a>
+        <a href="" className="hover:text-gray-300">Sign Up</a>
+        <a href="" className="hover:text-gray-300">About</a> 
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
