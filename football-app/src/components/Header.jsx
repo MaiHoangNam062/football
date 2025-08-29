085c776acb9b38deec06f000dd4edb3661654770
Nam Mai <maihoangnam.study@gmail.com>
2025-08-29 22:04:50 +0200

    Try Header

---
+++ football-app/src/components/Header.jsx
@@ -0,0 +1,9 @@
+function Header() {
+  return (
+    <header className="bg-gray-800 text-white p-4 shadow-md">
+      <h1 className="text-2xl font-bold text-center">My App Header</h1>
+    </header>
+  )
+}
+
+export default Header
